0746c3a7632578efdb0e677eae9fbe5dc1948636
ampmc-iscte <ampmc@iscte-iul.pt>
2023-05-15 16:39:18 +0100

    UI class

--- PA_Project/src/main/kotlin/UI.kt
+++ PA_Project/src/main/kotlin/UI.kt
@@ -2,4 +2,14 @@ class UI : Observer {
     override fun update(subject: Subject) {
         TODO("Not yet implemented")
     }
+
+    private val editor = Editor()
+
+    fun open() {
+        editor.open()
+    }
+}
+
+fun main() {
+    UI().open()
 }
