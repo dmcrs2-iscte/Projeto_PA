a01140fbe3fa6a09d15ee78facd4ab73227895bb
dmcrs2-iscte <dmcrs2@iscte-iul.pt>
2023-05-28 19:38:15 +0100

    Added undo and delete all buttons

--- PA_Project/src/main/kotlin/UI.kt
+++ PA_Project/src/main/kotlin/UI.kt
@@ -1,5 +1,7 @@
+import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.GridLayout
+import java.awt.event.MouseListener
 import java.util.*
 import javax.swing.*
 
@@ -12,20 +14,40 @@ class UI(private val jsonObject: JSONObject = JSONObject()) {
 
     private val frame = JFrame("Josue - JSON Object Editor").apply {
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        layout = GridLayout(0, 2)
+        layout = BorderLayout()
         size = Dimension(600, 600)
 
         val editorView = EditorView()
-        editorView.addObserver(object : EditorViewObserver {
-            override fun elementAdded(property: JSONProperty) = runCommand(AddElement(jsonObject, property))
-            override fun elementRemoved(property: JSONProperty) = runCommand(RemoveElement(jsonObject, property))
-            override fun elementReplaced(property: JSONProperty, newElement: JSONElement) = runCommand(ReplaceElement(jsonObject, property, newElement))
-            override fun allElementsRemoved() = runCommand(RemoveAllElements(jsonObject))
-        })
-        add(editorView)
+        add(JPanel().apply {
+            layout = GridLayout(0,2)
+            editorView.addObserver(object : EditorViewObserver {
+                override fun elementAdded(property: JSONProperty) = runCommand(AddElement(jsonObject, property))
+                override fun elementRemoved(property: JSONProperty) = runCommand(RemoveElement(jsonObject, property))
+                override fun elementReplaced(property: JSONProperty, newElement: JSONElement) =
+                    runCommand(ReplaceElement(jsonObject, property, newElement))
+            })
+            add(editorView)
 
-        val jsonView = JSONView(jsonObject)
-        add(jsonView)
+            val jsonView = JSONView(jsonObject)
+            add(jsonView)
+        }, BorderLayout.CENTER)
+
+        add(JPanel().apply {
+            layout = GridLayout(0,2)
+            add(JButton("Undo").apply {
+                addActionListener {
+                    if(!commands.isEmpty()) {
+                        commands.pop().undo()
+                        revalidate()
+                        repaint()
+                    }
+                    println(commands)
+                }
+            })
+            add(JButton("Delete All").apply {
+
+            })
+        }, BorderLayout.PAGE_END)
     }
 
     fun open() {
